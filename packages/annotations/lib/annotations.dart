6a1786c80abc61e014472654cf23b74a93cee9e8
wurikiji <wurikiji@gmail.com>
2022-10-21 21:44:22 +0900

    chore: remove unused files

--- packages/annotations/lib/annotations.dart
+++ packages/annotations/lib/annotations.dart
@@ -4,5 +4,3 @@
 library annotations;
 
 export 'src/getter_setter.dart';
-export 'src/injectable_annotation.dart';
-export 'src/responsive_widget_annotation.dart';
--- packages/annotations/lib/src/injectable_annotation.dart
+++
@@ -1,3 +0,0 @@
-class Injectable {
-  const Injectable();
-}
